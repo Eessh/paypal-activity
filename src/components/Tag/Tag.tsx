27ac451fc52b4ca3c619be979a9daac25a9a0467
Eessh <eesshkumarjagana@gmail.com>
2023-06-25 15:17:37 +0530

    ui fix: text size decrement for mobile screens

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -6,7 +6,7 @@ type TagPropsType = {
 const Tag = ({ text, active }: TagPropsType) => {
   return (
     <span
-      className={`inline-flex items-center rounded-full text-[#2b3375] ${active ? "bg-[#d1f2ff]" : "bg-gray-50 ring-gray-600/50"} px-4 py-2 text-base font-medium ring-1 ring-inset cursor-pointer`}
+      className={`inline-flex items-center rounded-full text-[#2b3375] ${active ? "bg-[#d1f2ff]" : "bg-gray-50 ring-gray-600/50"} px-4 py-2 text-sm sm:text-base font-medium ring-1 ring-inset cursor-pointer`}
     >
       {text}
     </span>
